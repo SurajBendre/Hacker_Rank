fcfaea5ea3d2df9b280552ae6538bf6bbd7799d0
Suraj <surajbendrepatil@gmail.com>
2023-12-14 20:54:17 +0530

    grpby

---
+++ itertools_groupby.py
@@ -0,0 +1,27 @@
+from itertools import groupby
+[print((len(list(j)),int(i)),end=' ') for i,j in groupby(input())]
+
+'''
+Input Format
+
+A single line of input consisting of the string .
+
+Output Format
+
+A single line of output consisting of the modified string.
+
+Constraints
+
+All the characters of  denote integers between  and .
+
+
+Sample Input
+
+1222311
+Sample Output
+
+(1, 1) (3, 2) (1, 3) (2, 1)
+Explanation
+
+First, the character  occurs only once. It is replaced by . Then the character  occurs three times, and it is replaced by  and so on.
+'''
